6445833c940b996ad2efbfe2c431903da43bb5ba
Surax98 <giacomo.surace@gmail.com>
2023-08-24 07:55:45 +0000

    fixed typo

--- pkg/interlink/create.go
+++ pkg/interlink/create.go
@@ -27,7 +27,7 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 
 	var retrieved_data []commonIL.RetrievedPodData
 	for _, pod := range req2 {
-    
+
 		data := []commonIL.RetrievedPodData{}
 		if commonIL.InterLinkConfigInst.ExportPodData {
 			data, err = getData(pod)
@@ -37,66 +37,64 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			log.G(Ctx).Debug(data)
-      
-		}
-		if check {
-			ToBeCreated = append(ToBeCreated, pod.Name)
-			data := []commonIL.RetrievedPodData{}
-			if commonIL.InterLinkConfigInst.ExportPodData {
-				data, err = getData(pod)
-				if err != nil {
-					statusCode = http.StatusInternalServerError
-					w.WriteHeader(statusCode)
-					return
-				}
-				log.G(Ctx).Debug(data)
-			}
 
-			if data == nil {
-				data = append(data, commonIL.RetrievedPodData{Pod: *pod})
+		}
+		data = []commonIL.RetrievedPodData{}
+		if commonIL.InterLinkConfigInst.ExportPodData {
+			data, err = getData(pod)
+			if err != nil {
+				statusCode = http.StatusInternalServerError
+				w.WriteHeader(statusCode)
+				return
 			}
+			log.G(Ctx).Debug(data)
+		}
+
+		if data == nil {
+			data = append(data, commonIL.RetrievedPodData{Pod: *pod})
+		}
 
 		retrieved_data = append(retrieved_data, data...)
-	}
 
-	if retrieved_data != nil {
-		bodyBytes, err = json.Marshal(retrieved_data)
-		log.G(Ctx).Debug(string(bodyBytes))
-		reader := bytes.NewReader(bodyBytes)
+		if retrieved_data != nil {
+			bodyBytes, err = json.Marshal(retrieved_data)
+			log.G(Ctx).Debug(string(bodyBytes))
+			reader := bytes.NewReader(bodyBytes)
 
-		req, err = http.NewRequest(http.MethodPost, commonIL.InterLinkConfigInst.Sidecarurl+":"+commonIL.InterLinkConfigInst.Sidecarport+"/create", reader)
+			req, err = http.NewRequest(http.MethodPost, commonIL.InterLinkConfigInst.Sidecarurl+":"+commonIL.InterLinkConfigInst.Sidecarport+"/create", reader)
 
-		if err != nil {
-			statusCode = http.StatusInternalServerError
-			w.WriteHeader(statusCode)
-			log.G(Ctx).Fatal(err)
-		}
-
-		log.G(Ctx).Info("InterLink: forwarding Create call to sidecar")
-		var resp *http.Response
-		for {
-			resp, err = http.DefaultClient.Do(req)
 			if err != nil {
-				log.G(Ctx).Error(err)
-				time.Sleep(time.Second * 5)
-			} else {
-				break
+				statusCode = http.StatusInternalServerError
+				w.WriteHeader(statusCode)
+				log.G(Ctx).Fatal(err)
 			}
+
+			log.G(Ctx).Info("InterLink: forwarding Create call to sidecar")
+			var resp *http.Response
+			for {
+				resp, err = http.DefaultClient.Do(req)
+				if err != nil {
+					log.G(Ctx).Error(err)
+					time.Sleep(time.Second * 5)
+				} else {
+					break
+				}
+			}
+
+			statusCode = resp.StatusCode
+
+			if resp.StatusCode == http.StatusOK {
+				statusCode = http.StatusOK
+				log.G(Ctx).Debug(statusCode)
+			} else {
+				statusCode = http.StatusInternalServerError
+				log.G(Ctx).Debug(statusCode)
+			}
+
+			returnValue, _ := ioutil.ReadAll(resp.Body)
+			log.G(Ctx).Debug(string(returnValue))
+			w.WriteHeader(statusCode)
+			w.Write(returnValue)
 		}
-
-		statusCode = resp.StatusCode
-
-		if resp.StatusCode == http.StatusOK {
-			statusCode = http.StatusOK
-			log.G(Ctx).Debug(statusCode)
-		} else {
-			statusCode = http.StatusInternalServerError
-			log.G(Ctx).Debug(statusCode)
-		}
-
-		returnValue, _ := ioutil.ReadAll(resp.Body)
-		log.G(Ctx).Debug(string(returnValue))
-		w.WriteHeader(statusCode)
-		w.Write(returnValue)
 	}
 }
