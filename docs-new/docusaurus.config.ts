5b7435f0423aeaf9ea137ac613e4a4e800865f65
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2023-12-30 11:50:33 +0100

    update docusaurus metadata

--- docs-new/docusaurus.config.ts
+++ docs-new/docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/img/interlink_logo.png',
     navbar: {
       title: 'Home',
       logo: {
--- docs-new/src/pages/index.tsx
+++ docs-new/src/pages/index.tsx
@@ -34,8 +34,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`interLink`}
+      description="Virtual Kubelets for everyone">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
