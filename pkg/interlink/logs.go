c20308efc4f947714714e7e69f9893080f5019b1
Surax98 <giacomo.surace@gmail.com>
2024-02-20 09:50:21 +0000

    minors

--- pkg/interlink/logs.go
+++ pkg/interlink/logs.go
@@ -54,7 +54,7 @@ func (h *InterLinkHandler) GetLogsHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 	reader := bytes.NewReader(bodyBytes)
-	req, err := http.NewRequest(http.MethodPost, h.Config.Sidecarurl+":"+h.Config.Sidecarport+"/getLogs", reader)
+	req, err := http.NewRequest(http.MethodGet, h.Config.Sidecarurl+":"+h.Config.Sidecarport+"/getLogs", reader)
 	if err != nil {
 		log.G(Ctx).Fatal(err)
 	}
