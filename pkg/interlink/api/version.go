815ed0961a8d8db46b9032d2f2204fb06df3f57e
w3lld1 <42353747+w3lld1@users.noreply.github.com>
2026-07-30 03:32:52 +0200

    feat: expose the running server version (#544)
    Signed-off-by: w3lld1 <42353747+w3lld1@users.noreply.github.com>

---
+++ pkg/interlink/api/version.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// VersionHandler reports the version of the running interLink server.
+func VersionHandler(version string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(struct {
+			Version string `json:"version"`
+		}{Version: version})
+	}
+}
